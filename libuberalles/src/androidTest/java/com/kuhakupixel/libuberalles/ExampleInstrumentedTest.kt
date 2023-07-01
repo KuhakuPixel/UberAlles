80ed73a557b10f9b4cf76607b8d12ba36d3205f6
kuhakupixel <nicholaspixels@gmail.com>
2023-07-01 15:10:20 +0700

    turn libuberalles's module to android library instead of normal app

--- libuberalles/src/androidTest/java/com/kuhakupixel/libuberalles/ExampleInstrumentedTest.kt
+++ libuberalles/src/androidTest/java/com/kuhakupixel/libuberalles/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kuhakupixel.libuberalles", appContext.packageName)
+        assertEquals("com.kuhakupixel.libuberalles.test", appContext.packageName)
     }
 }
